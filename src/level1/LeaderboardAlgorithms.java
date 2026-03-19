e9b7284874386aa62a0849ea790750a211bc83b2
kanemoto.k <kanemoto.k@K99470BNE202.mccd.edu>
2026-03-19 10:03:12 -0700

    added insertion sort

--- src/level1/LeaderboardAlgorithms.java
+++ src/level1/LeaderboardAlgorithms.java
@@ -9,37 +9,74 @@ import java.util.ArrayList;
  */
 public class LeaderboardAlgorithms {
 
-    /**
-     * TODO: Sort the list by score DESCENDING (highest score first).
-     *
-     * Requirements:
-     * - Sort IN PLACE (modify the given list)
-     * - You may use Collections.sort + Comparator for the first version.
-     * - Week 8 challenge/extra credit: replace with merge sort or quicksort.
-     */
-    public static void sortByScoreDescending(ArrayList<ScoreEntry> list) {
-        // TODO
-    }
+	/**
+	 * TODO: Sort the list by score DESCENDING (highest score first).
+	 *
+	 * Requirements:
+	 * - Sort IN PLACE (modify the given list)
+	 * - You may use Collections.sort + Comparator for the first version.
+	 * - Week 8 challenge/extra credit: replace with merge sort or quicksort.
+	 */
+	public static void sortByScoreDescending(ArrayList<ScoreEntry> list) {
+		// TODO
+		//for (int i = 1; i < numbers.length; i++) {
+		for (int i = 1; i < list.size(); i++) {
+			int j = i;
+			// Insert numbers[i] into sorted part
+			// stopping once numbers[i] in correct position
+			//while (j > 0 && numbers[j] < numbers[j - 1]) {
+			while (j > 0 && list.get(j).getScore() < list.get(j-1).getScore()) {
 
-    /**
-     * TODO: Sort the list by username ASCENDING (A -> Z).
-     *
-     * Requirements:
-     * - Sort IN PLACE (modify the given list)
-     */
-    public static void sortByUsernameAscending(ArrayList<ScoreEntry> list) {
-        // TODO
-    }
+				// Swap numbers[j] and numbers [j - 1]
+				//int temp = numbers[j];
+				ScoreEntry temp = list.get(j);
+				//numbers[j] = numbers[j - 1];
+				list.set(j,list.get(j-1));
+				//numbers[j - 1] = temp;
+				list.set(j-1, temp);
+				j--;
+			}
+		}
 
-    /**
-     * TODO: Binary search for an EXACT username match.
-     *
-     * Precondition: list must already be sorted by username ascending!
-     *
-     * @return index of the matching entry, or -1 if not found
-     */
-    public static int binarySearchByUsername(ArrayList<ScoreEntry> list, String username) {
-        // TODO
-        return -1;
-    }
+	}
+
+	/**
+	 * TODO: Sort the list by username ASCENDING (A -> Z).
+	 *
+	 * Requirements:
+	 * - Sort IN PLACE (modify the given list)
+	 */
+	public static void sortByUsernameAscending(ArrayList<ScoreEntry> list) {
+		// TODO
+		//for (int i = 1; i < numbers.length; i++) {
+		for (int i = 1; i < list.size(); i++) {
+			int j = i;
+			// Insert numbers[i] into sorted part
+			// stopping once numbers[i] in correct position
+			//while (j > 0 && numbers[j] < numbers[j - 1]) {
+			while (j > 0 && list.get(j).getScore() > list.get(j-1).getScore()) {
+
+				// Swap numbers[j] and numbers [j - 1]
+				//int temp = numbers[j];
+				ScoreEntry temp = list.get(j);
+				//numbers[j] = numbers[j - 1];
+				list.set(j,list.get(j-1));
+				//numbers[j - 1] = temp;
+				list.set(j-1, temp);
+				j--;
+			}
+		}
+	}
+
+	/**
+	 * TODO: Binary search for an EXACT username match.
+	 *
+	 * Precondition: list must already be sorted by username ascending!
+	 *
+	 * @return index of the matching entry, or -1 if not found
+	 */
+	public static int binarySearchByUsername(ArrayList<ScoreEntry> list, String username) {
+		// TODO
+		return -1;
+	}
 }
